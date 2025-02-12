facdae83ffedf7d94caffaa07fc6593ac1c6b25c
mangonam <hyeonkyu775@gmail.com>
2025-02-12 17:48:37 +0900

    feat :  modifying

---
+++ src/main/java/chapter11_arrays/ArrayTest01.java
@@ -0,0 +1,10 @@
+package chapter11_arrays;
+
+public class ArrayTest01 {
+    public static void main(String[] args) {
+        int[] arr1 = new int[5];
+        int[] arr2 = {1,2,3,4,5};
+        System.out.println(arr1);
+        System.out.println(arr2);
+    }
+}
